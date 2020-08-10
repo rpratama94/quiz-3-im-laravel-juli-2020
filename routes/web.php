94dc5ae662b9196e7ff850f0de6bb2265e4c3b56
rpratama94 <rpratama.sky@gmail.com>
2020-08-10 22:23:57 +0700

    Quiz 3

--- routes/web.php
+++ routes/web.php
@@ -19,10 +19,11 @@ Route::get('/', function () {
     return view('dasboard');
 });
 
-Route::get('/items/create', 'ItemController@create'); // menampilkan halaman form
-Route::post('/items', 'ItemController@store'); // menyimpan data
-Route::get('/items', 'ItemController@index'); // menampilkan semua
-Route::get('/items/{id}', 'ItemController@show'); // menampilkan detail item dengan id 
-Route::get('/items/{id}/edit', 'ItemController@edit'); // menampilkan form untuk edit item
-Route::put('/items/{id}', 'ItemController@update'); // menyimpan perubahan dari form edit
-Route::delete('/items/{id}', 'ItemController@destroy'); // menghapus data dengan id
+
+Route::get('/proyek', 'ItemController@create'); // menampilkan halaman form
+Route::get('/proyek/create', 'ItemController@create'); // membuat proyek nikah
+Route::get('/proyek', 'ItemController@index'); // menampilkan semua
+Route::get('/proyek/{id}', 'ItemController@show'); // menampilkan detail item dengan id 
+Route::get('/proyek/{id}/edit', 'ItemController@edit'); // menampilkan form untuk edit item
+Route::put('/proyek/{id}', 'ItemController@update'); // menyimpan perubahan dari form edit
+Route::delete('/proyek/{id}', 'ItemController@destroy'); // menghapus data dengan id
